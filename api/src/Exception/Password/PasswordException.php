fe98b10100fc9bec2ec80528daca2bb017b1894a
oxiEva <eva.lujan@holaluz.com>
2021-10-03 13:09:28 +0200

    Change password skeleton

--- api/src/Exception/Password/PasswordException.php
+++ api/src/Exception/Password/PasswordException.php
@@ -7,9 +7,13 @@ use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class PasswordException extends BadRequestHttpException
 {
-
     public static function invalidLength(): self
     {
         throw new self('Password must have at least 6 characters');
+    }
+
+    public static function oldPasswordDoesNotMatch(): self
+    {
+        throw new self('Old password does not match');
     }
 }
